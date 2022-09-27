f21acf3501e6e1b188d0098baf72cb4afbc61029
sureshkumarps006 <91891449+sureshkumarps006@users.noreply.github.com>
2022-09-27 11:19:10 +0530

    Add files via upload

---
+++ Patternmatching.java
@@ -0,0 +1,39 @@
+package com.codebind;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class Patternmatching {
+
+	public static void main(String[] args) {
+		// TODO Auto-er method stub
+
+		//Pattern p = Pattern.compile(".ab");
+		/*Pattern p = Pattern.compile("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$");
+		Matcher m = p.matcher("abb");
+		boolean b = m.matches();
+		System.out.println(b);*/
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your email:");
+		String email= sc.next();
+		String reg ="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]++$";
+		/*Pattern p = Pattern.compile(reg);
+		Matcher m = p.matcher(email);
+		boolean b = m.matches();*/
+		boolean result = email.matches(reg);
+		if(result) {
+			System.out.print(result);
+		}
+		else
+		{
+			System.out.print(result);
+		}
+		
+		
+		
+		
+	}
+
+}
