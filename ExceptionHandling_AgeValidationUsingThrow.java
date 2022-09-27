69eea251de4971d16d3484d67e07911a318a4162
sureshkumarps006 <91891449+sureshkumarps006@users.noreply.github.com>
2022-09-27 17:26:04 +0530

    Add files via upload

---
+++ ExceptionHandling_AgeValidationUsingThrow.java
@@ -0,0 +1,25 @@
+package com.codebind;
+
+public class ExceptionHandling_AgeValidationUsingThrow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		try {
+			int age = 14;
+			if(age<18) {
+				throw new ArithmeticException();
+			}
+			else {
+				System.out.println("Your eligible");
+			}
+			}
+			catch (ArithmeticException e) {
+				// TODO: handle exception
+				System.out.println(e);
+			}
+
+
+	}
+
+}
