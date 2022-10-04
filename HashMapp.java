339f46cfd324449434f0d2e9df78bf6d0bfcb436
sureshkumarps006 <91891449+sureshkumarps006@users.noreply.github.com>
2022-10-04 11:00:18 +0530

    Add files via upload

---
+++ HashMapp.java
@@ -0,0 +1,42 @@
+package com.codebind;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap <Integer,String> map=  new HashMap();
+		
+		map.put(1, "Helo");
+		map.put(2, "Hello");
+		map.put(3, "Had");
+		map.put(4, "How");
+		map.put(5, "Hi");
+		
+		System.out.println(map.get(3));
+		System.out.println(map);
+		/*
+		 * for(Map.Entry entry:map.entrySet() ){ String val=entry.getValue().toString();
+		 * if(val.equals("Had")) { int key=(int) entry.getKey();
+		 * System.out.println(key); break; }
+		 * 
+		 * }
+		 */
+
+	//OR	
+		for(Map.Entry<Integer,String> entry:map.entrySet() ){
+			String val=entry.getValue();
+			if(val.equals("Had"))
+			{
+				int key= entry.getKey();
+				System.out.println(key);
+				break;
+			}
+			
+		}
+		
+	}
+
+}
---
+++ TreeMapp.java
@@ -0,0 +1,34 @@
+package com.codebind;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class TreeMapp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map <Integer,String> map=  new TreeMap();
+		
+		map.put(1, "Helo");
+		map.put(2, "Hello");
+		map.put(3, "Had");
+		map.put(4, "How");
+		map.put(5, "Hi");
+		
+		System.out.println(map.get(3));
+		System.out.println(map);
+			
+		for(Map.Entry<Integer,String> entry:map.entrySet() ){
+			String val=entry.getValue();
+			if(val.equals("Had"))
+			{
+				int key= entry.getKey();
+				System.out.println(key);
+				break;
+			}
+			
+		}
+		
+	}
+
+}
